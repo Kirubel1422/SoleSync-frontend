e67e9cb58fcd049ee5d844dc1c82b1055139f40d
Kirubel <kirubel739@gmail.com>
2024-07-27 01:57:47 +0400

    build(app.jsx): added Shoe-Detail and Cart page
    Also added on pathname change to scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import { lazy, Suspense } from "react";
-import { Routes, Route } from "react-router-dom";
+import { lazy, Suspense, useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import UserNav from "./components/Navigation/UserNav";
 import Footer from "./components/Footer/Footer";
 import { twMerge } from "tailwind-merge";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Signup = lazy(() => import("./pages/Signup"));
 const Login = lazy(() => import("./pages/Login"));
 const Home = lazy(() => import("./pages/Home"));
+const ShoeDetail = lazy(() => import("./pages/ShoeDetail"));
+const Cart = lazy(() => import("./pages/Cart"));
 
 function App() {
+  const location = useLocation();
+  useEffect(() => {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  }, [location.pathname]);
+
   return (
     <Suspense fallback={<div>Loading ...</div>}>
       <header className={twMerge("mt-5 px-[62px]")}>
@@ -20,6 +32,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/shoe-detail/:id" element={<ShoeDetail />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </main>
 
